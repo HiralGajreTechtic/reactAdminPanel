b17bd52fc633aab833b79a680ee3dad527b4914b
HiralGajre <techtic.hiralgajre@gmail.com>
2023-07-06 18:45:28 +0530

    changes related to axios

---
+++ src/Component/AxoisDemo/AxiosDemo.js
@@ -0,0 +1,118 @@
+//crud
+//input field, add button, table list
+// return list in table
+// edit and delete
+
+import React, { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Row, Container } from 'react-bootstrap';
+import axios from "axios";
+
+console.log("here==");
+const AxiosDemo = () => {
+    const [toDo, setToDo] = useState([]);
+    const [isEdit, setEdit] = useState(false);
+    const [updateIndex, setUpdateIndex] = useState();
+    const [title, setTitle] = useState();
+    console.log("hi=");
+    useEffect(() => {
+        console.log("entered==!")
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
+                console.log("res=", res);
+                setToDo(res)
+            }).catch((err) => {
+                console.log("err=", err);
+            })
+    }, []);
+
+    function insertToDo(toDoTitle) {
+        if (isEdit) {
+            var updateRecord = toDo.map((obj) => {
+                if (obj.id === updateIndex) {
+                    obj.title = toDoTitle;
+                }
+                return obj;
+            });
+            setToDo(updateRecord);
+        } else {
+            const id = Math.floor(Math.random() * 100);
+            setToDo([
+                ...toDo,
+                {
+                    id: id,
+                    title: toDoTitle
+                },
+            ]);
+        }
+        setEdit(false);
+    }
+    const handleDelete = (index, e) => {
+        setToDo(toDo.filter((v, i) => i !== index));
+    }
+    const handleEdit = (index, data) => {
+        setUpdateIndex(data.id)
+        setTitle(data.title)
+        setEdit(!isEdit);
+
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setTitle(e.target.value);
+    }
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" value={title} onChange={handleChange} placeholder="Enter to do" />
+
+                        </Form.Group>
+
+                        <Button variant="primary" onClick={() => {
+                            insertToDo(title);
+                        }}>
+                            Submit
+                        </Button >
+                    </Form>
+                </Row>
+                <Row>
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>To Do title</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* {{ toDo }} */}
+                            {/* {
+                                toDo.map((data, index) => (
+                                    <tr key={index}>
+                                        <td>{data.id}</td>
+                                        <td>{data.name}</td>
+                                        <td>
+                                            <button className="btn btn-success btn-xs" onClick={e => handleEdit(index, data)}>Edit</button>
+                                            <button className="btn btn-danger btn-xs" onClick={e => handleDelete(index, e)}>Delete</button> </td>
+                                    </tr>
+                                ))
+                            } */}
+                        </tbody>
+                    </Table>
+                </Row>
+            </Container>
+
+
+        </>
+    )
+
+}
+
+export default AxiosDemo;
--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
-import {LinkContainer} from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import Login from '../Login/Login';
 import ProtectedRoute from '../RouteProtection/ProtectedRoute';
 import Home from '../Home/Home';
@@ -12,69 +12,77 @@ import Counter from '../Counter/Counter';
 import ToDo from '../ToDo/ToDo';
 import ProfileForm from '../Profile/ProfileForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AxiosDemo from '../AxoisDemo/AxiosDemo';
 
-const NavBar =()=>{
-    return <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Admin Panel</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <div className="row col-md-12 div12">
+const NavBar = () => {
+  return <>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Admin Panel</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div className="row col-md-12 div12">
       <div className="col-md-3 div3">
         <Nav className="col-md-12 d-none d-md-block bg-dark sidebar"
-              
-              >
+
+        >
           <div className="sidebar-sticky"></div>
           <Nav.Item>
             <LinkContainer to="/">
-            <Nav.Link >Home</Nav.Link>
+              <Nav.Link >Home</Nav.Link>
             </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
+          </Nav.Item>
+          <Nav.Item>
+            <LinkContainer to="/axiosDemo">
+              <Nav.Link >Axios demo</Nav.Link>
+            </LinkContainer>
+          </Nav.Item>
+          <Nav.Item></Nav.Item>
+          <Nav.Item>
             <LinkContainer to="/profile">
-            <Nav.Link >Profile</Nav.Link>
+              <Nav.Link >Profile</Nav.Link>
             </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
+          </Nav.Item>
+          <Nav.Item>
             <LinkContainer to="/login">
-            <Nav.Link >Login</Nav.Link>
+              <Nav.Link >Login</Nav.Link>
             </LinkContainer>
-            </Nav.Item>
-          </Nav>
+          </Nav.Item>
+        </Nav>
       </div>
       <div className='col-md-9'>
-      <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="/counterApp" element={<ProtectedRoute> <Counter/> </ProtectedRoute>}  />
-                <Route path="/toDoApp" element={<ProtectedRoute> <ToDo /></ProtectedRoute>} />
-                  <Route path="/profile" element={<ProtectedRoute>  <ProfileForm /> </ProtectedRoute>} />
-                  <Route path="/login" element={<Login />} />
-              </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counterApp" element={<ProtectedRoute> <Counter /> </ProtectedRoute>} />
+          <Route path="/toDoApp" element={<ProtectedRoute> <ToDo /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute>  <ProfileForm /> </ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/axiosDemo" element={<AxiosDemo />} />
+        </Routes>
       </div>
     </div>
   </>
